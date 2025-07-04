20f911bf2def5ee3f27ee8c5e193e26b2d620955
MichelliDami <146675803+MichelliDami@users.noreply.github.com>
2025-07-03 21:25:05 -0300

    Add files via upload

---
+++ ListaDeCompra2706/TrabalhoDeCompra/Models/Dtos/ItemCompraDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TrabalhoDeCompra.Models.Dtos
+{
+    public class ItemCompraDto
+    {
+        public string Nome { get; set; }
+        public int Prioridade { get; set; }
+
+        public string LocalItem { get; set; }
+
+        public int Quantidade { get; set; }
+
+    }
+}
---
+++ ListaDeCompra2706/TrabalhoDeCompra/Models/View/ItemCompraView.cs
@@ -0,0 +1,13 @@
+﻿namespace TrabalhoDeCompra.Models.View
+{
+    public class ItemCompraView
+    {
+        public string Nome { get; set; }
+        public int Prioridade { get; set; }
+
+        public string LocalItem { get; set; }
+
+        public int Quantidade { get; set; }
+        public Guid Id { get; set; }
+    }
+}
